3b6355775c8b0c0b945f98c85d72832f359694da
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-02-23 00:12:23 -0600

    🎉 Publish v0.1

---
+++ components/head.js
@@ -0,0 +1,31 @@
+import Head from 'next/head'
+
+export default function HeadObject({children}) {
+    const title = "The Perfect Next.js Theme";
+    const description = "This is the perfect Next.js theme that you should use instead of create-next-app.";
+    const keywords = "put some fun keywords here";
+    const author = "Sarthak Mohanty";
+    const twitter = "@sarthaktexas";
+    //const image = "/ogimage.png"; // This is your OpenGraph image
+    return (
+        <Head>
+            <meta charSet="utf-8" />
+            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width,initial-scale=1" />
+            <title>{title}</title>
+            <meta name="description" content={description} />
+            <meta name="keywords" content={keywords} />
+            <meta name="author" content={author} />
+            <meta property="og:url" content="put it in this quotes" /> {/* This is where you put the domain */}
+            <meta property="og:type" content="website" />
+            <meta property="og:title" content={title} />
+            <meta property="og:description" content={description} />
+            <meta property="og:image" content={image} />
+            <meta name="twitter:card" content="summary" />
+            <meta name="twitter:site" content={twitter} />
+            <meta name="twitter:creator" content={twitter} />
+            {/* Add analytics here */}
+            {children}
+        </Head>
+    )
+}
