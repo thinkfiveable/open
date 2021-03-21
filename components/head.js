49466d27b4afc763b6c85a86ebcc95ac8f436d9e
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-03-21 01:24:41 -0500

    fix a quick comment thing

--- components/head.js
+++ components/head.js
@@ -16,8 +16,7 @@ export default function HeadObject({ children }) {
             <meta name="description" content={description} />
             <meta name="keywords" content={keywords} />
             <meta name="author" content={author} />
-            <meta property="og:url" content="put it in this quotes" />
-            {/* This is where you put the domain */}
+            <meta property="og:url" content="put the domain in quotes here" />
             <meta property="og:type" content="website" />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={description} />
