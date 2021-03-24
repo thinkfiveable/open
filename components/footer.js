d0f02e781f8a6944c86fb25a583c4354f3c9e689
ceelo777 <lochristopher9@gmail.com>
2021-03-24 15:28:35 -0400

    fix: organized classes into custom ones using @apply, removed cntl

--- components/footer.js
+++ components/footer.js
@@ -1,36 +1,4 @@
-import cntl from "cntl";
-
 export default function Footer() {
-    const footerCN = cntl`
-        flex 
-        flex-col
-        md:flex-row
-        justify-center
-        align-center      
-        items-center
-        justify-items-center
-
-        md:h-20
-        sm:h-60
-        h-40
-        bg-black
-        
-        text-white        
-        font-medium                      
-
-        2xl:text-2xl
-        xl:text-xl
-        lg:text-lg
-        md:text-md
-        sm:text-sm
-        text-xs        
-
-        2xl:gap-20
-        xl:gap-15
-        lg:gap-10
-        md:gap-5        
-        gap-2
-    `;
     return (
         <div className="footer">
             <svg id="footer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 100 1440 200">
@@ -40,7 +8,7 @@ export default function Footer() {
                     d="M0,288L40,266.7C80,245,160,203,240,176C320,149,400,139,480,138.7C560,139,640,149,720,165.3C800,181,880,203,960,197.3C1040,192,1120,160,1200,149.3C1280,139,1360,149,1400,154.7L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
                 />
             </svg>
-            <div className={footerCN}>
+            <div className="footer-gap footer-font footer-div footer-text footer-flex">
                 <div>&#169; Fiveable 2021 | All Rights Reserved.</div>
                 <div>Docs</div>
                 <div>Contributors</div>
