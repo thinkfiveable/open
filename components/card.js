2f9c8dc3ddb0d790e7d5b8a03f4f82fa07bf9412
Ella <72365100+eilla1@users.noreply.github.com>
2021-03-27 13:34:31 -0700

    fix: enlarge cards and grid gap (#28)
    * fix: enlarge cards and grid gap
    
    addressed styling issues with card size and spacing between cards
    
    fixes Styling issues with Variant 1 of Card #26
    
    * fix: grid responsiveness and card sizes
    
    * fix: center grid items
    
    Co-authored-by: Sarthak Mohanty <sarthaktexas@gmail.com>
    
    Co-authored-by: Sarthak Mohanty <sarthaktexas@gmail.com>

--- components/card.js
+++ components/card.js
@@ -1,10 +1,12 @@
 export default function Card({ name, title, projectType, thumbnail, profileIcon, award }) {
     return (
         <div className="py-4">
-            <div style={{ borderColor: award }} className="rounded-xl h-40 w-72 overflow-hidden border-4">
+            <div
+                style={{ borderColor: award }}
+                className="rounded-xl h-52 w-80 sm:w-3/4 md:w-80 overflow-hidden border-4 mx-auto">
                 <img src={thumbnail} alt="featured project" />
             </div>
-            <div className="flex flex-row pt-4 justify-between max-w-6xl">
+            <div className="flex flex-row pt-4 justify-around">
                 <div className="flex flex-col">
                     <h2 className="font-rubik font-medium text-xl">{title}</h2>
                     <div className="flex flex-row">
@@ -13,7 +15,7 @@ export default function Card({ name, title, projectType, thumbnail, profileIcon,
                         </p>
                     </div>
                 </div>
-                <img src={profileIcon} className="rounded-full h-12 w-12" alt="Profile" />
+                <img src={profileIcon} className="rounded-full h-12 w-12 float-right" alt="Profile" />
             </div>
         </div>
     );
