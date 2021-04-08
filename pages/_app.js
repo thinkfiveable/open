8a308a77579fac98c244fa7610b13dfac8c2b885
Ella <72365100+eilla1@users.noreply.github.com>
2021-04-08 16:24:48 -0700

    feat: add mdx and about page (#36)

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,17 @@
 import "../styles/globals.css";
 
+import { MDXProvider } from "@mdx-js/react";
 import { ThemeProvider } from "next-themes";
+
+import MDXComponents from "../components/MDXComponents";
 
 function MyApp({ Component, pageProps }) {
     return (
         <ThemeProvider defaultTheme="system" attribute="class">
-            {/* eslint-disable-next-line */}
-            <Component {...pageProps} />
+            <MDXProvider components={MDXComponents}>
+                {/* eslint-disable-next-line */}
+                <Component {...pageProps} />
+            </MDXProvider>
         </ThemeProvider>
     );
 }
