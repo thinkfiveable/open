7833e31412c87a487c559fc217a33b69013fab73
Sarthak Mohanty <sarthaktexas@gmail.com>
2021-04-08 19:16:02 -0500

    fix: bad class list, flexbox layout, hover on <a>

--- components/footer.js
+++ components/footer.js
@@ -8,26 +8,23 @@ export default function Footer() {
                     d="M0,288L40,266.7C80,245,160,203,240,176C320,149,400,139,480,138.7C560,139,640,149,720,165.3C800,181,880,203,960,197.3C1040,192,1120,160,1200,149.3C1280,139,1360,149,1400,154.7L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
                 />
             </svg>
-            <div
-                className="
-                2xl:text-2xl xl:text-xl lg:text-lg md:text-base sm:text-sm text-xs
-                2xl:gap-20 xl:gap-10 lg:gap-10 md:gap-5 gap-2
-                md:h-20 sm:h-60 h-40 bg-black
-                text-white font-medium
-                flex flex-col md:flex-row justify-center items-center justify-items-center
-            ">
-                <div>&#169; Fiveable 2021 | All Rights Reserved.</div>
-                <div>Docs</div>
-                <div>Contributors</div>
-                <div>
-                    <a href="https://hi.fiveable.me/contact/">Contact Us</a>
+            <div className="bg-black text-white font-medium flex flex-col space-y-5 md:flex-row md:space-y-0 py-5">
+                <div className="flex-1- flex justify-between items-center pl-4 md:pl-0">
+                    <div>&#169; Fiveable 2021 | All Rights Reserved.</div>
                 </div>
-                <div>
-                    <a href="https://hi.fiveable.me/privacy-policy/">Privacy</a>
-                </div>
-                <div>
-                    <a href="https://hi.fiveable.me/terms-of-use/">Terms and Conditions</a>
-                </div>
+                <ul className="flex flex-col content-start md:flex-row space-y-5 md:space-x-5 md:space-y-0">
+                    <li>Docs</li>
+                    <li>Contributors</li>
+                    <li>
+                        <a className="hover:bg-white hover:bg-opacity-5 px-4 py-2 rounded-xl" href="https://hi.fiveable.me/contact/">Contact Us</a>
+                    </li>
+                    <li>
+                        <a href="https://hi.fiveable.me/privacy-policy/">Privacy</a>
+                    </li>
+                    <li>
+                        <a href="https://hi.fiveable.me/terms-of-use/">Terms and Conditions</a>
+                    </li>
+                </ul>
             </div>
         </div>
     );
