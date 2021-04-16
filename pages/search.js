298fa9fbcde08ac79f168556bd12f2abf2ce5b39
Jason Antwi-Appah <hey@jasonaa.me>
2021-04-16 16:14:01 -0500

    feat: connect search page to data

--- pages/search.js
+++ pages/search.js
@@ -1,49 +1,57 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+import BackButton from "../components/backbtn";
 import HeadObject from "../components/head";
-import SearchBar from "../components/searchbar";
 import SearchCard from "../components/searchcard";
+import data from "../data/PROJECTS.json";
+import SearchData from "../lib/search";
 
 export default function Search() {
+    const router = useRouter();
+    const [searchVal, updateSearch] = useState(router.query.q || "");
+    const results = SearchData(searchVal, data);
     return (
         <div>
             <HeadObject />
             <main>
                 <div className="flex flex-col mx-5 sm:mx-20 justify-start gap-12 py-4 sm:py-12">
-                    <SearchBar />
-                    <SearchCard
-                        name="Student Name"
-                        title="Lorem Ipsum"
-                        projectType="Website"
-                        thumbnail="https://source.unsplash.com/random"
-                        profileIcon="https://picsum.photos/200"
-                    />
-                    <SearchCard
-                        name="Student Name"
-                        title="Lorem Ipsum"
-                        projectType="Website"
-                        thumbnail="https://source.unsplash.com/random"
-                        profileIcon="https://picsum.photos/200"
-                    />
-                    <SearchCard
-                        name="Student Name"
-                        title="Lorem Ipsum"
-                        projectType="Website"
-                        thumbnail="https://source.unsplash.com/random"
-                        profileIcon="https://picsum.photos/200"
-                    />
-                    <SearchCard
-                        name="Student Name"
-                        title="Lorem Ipsum"
-                        projectType="Website"
-                        thumbnail="https://source.unsplash.com/random"
-                        profileIcon="https://picsum.photos/200"
-                    />
-                    <SearchCard
-                        name="Student Name"
-                        title="Lorem Ipsum"
-                        projectType="Website"
-                        thumbnail="https://source.unsplash.com/random"
-                        profileIcon="https://picsum.photos/200"
-                    />
+                    <div className="flex flex-col-reverse items-start sm:items-center sm:flex-row sm:justify-between">
+                        <input
+                            type="text"
+                            className="lg:w-2/5 sm:w-1/2 p-2 lg:text-4xl md:text-3xl font-rubik border-b-4"
+                            placeholder="Enter your query..."
+                            name="search"
+                            autoComplete="off"
+                            value={searchVal}
+                            onChange={(evt) => {
+                                updateSearch(evt.target.value);
+                                router.replace(`/search?q=${evt.target.value}`);
+                            }}
+                        />
+                        <BackButton />
+                    </div>
+
+                    {results.length > 0 ? (
+                        results.map(({ repoName, repoOwner, projectType, repoLogo, repoOwnerIcon }) => {
+                            return (
+                                <SearchCard
+                                    key={repoName}
+                                    name={repoOwner}
+                                    title={repoName}
+                                    projectType={projectType}
+                                    thumbnail={repoLogo}
+                                    profileIcon={repoOwnerIcon}
+                                />
+                            );
+                        })
+                    ) : (
+                        <p className="px-2 text-xl font-rubik">
+                            {searchVal === ""
+                                ? "Enter your search above."
+                                : "Nothing was found for this query."}
+                        </p>
+                    )}
                 </div>
             </main>
         </div>
