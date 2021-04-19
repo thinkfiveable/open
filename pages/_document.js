2dd0aceb79b9b84ae77385714f12334964d92e68
Aaron Levin <awlevin@users.noreply.github.com>
2021-04-19 17:13:24 -0500

    chore: google-site-verification meta tag (#53)

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+export default class CustomDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <meta
+                        name="google-site-verification"
+                        content="Y_ap0oZB97bXXcmzOpaE1RL9JirZVSVrbELjoIAGyU4"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
