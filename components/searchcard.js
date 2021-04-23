63cb79221b35a72361d3c71be7dd8d9bd6459e84
Zaid <me@zaid-arshad.tech>
2021-04-22 20:57:30 -0400

    fix: search card broken images (#54)

--- components/searchcard.js
+++ components/searchcard.js
@@ -1,4 +1,4 @@
-export default function SearchCard({ name, title, projectType, thumbnail, profileIcon, award }) {
+export default function SearchCard({ repoOwner, title, projectType, thumbnail, award }) {
     return (
         <div className="flex sm:flex-row flex-col py-4">
             <div
@@ -15,14 +15,16 @@ export default function SearchCard({ name, title, projectType, thumbnail, profil
                     <h2 className="font-rubik font-medium md:text-5xl sm:text-3xl">{title}</h2>
                     <div className="flex flex-row">
                         <p className="md:text-3xl sm:text-xl font-mono">
-                            {name} • {projectType}
+                            {repoOwner} • {projectType}
                         </p>
                     </div>
-                    <img
-                        src={profileIcon}
-                        className="sm:my-5 my-2 rounded-full sm:h-16 sm:w-16 h-8 w-8 float-right"
-                        alt="Profile"
-                    />
+                    <a href={`https://github.com/${repoOwner}`}>
+                        <img
+                            src={`https://avatars.githubusercontent.com/${repoOwner}`}
+                            className="sm:my-5 my-2 rounded-full sm:h-16 sm:w-16 h-8 w-8 float-left"
+                            alt="Profile"
+                        />
+                    </a>
                 </div>
             </div>
         </div>
