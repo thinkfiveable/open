49d1812b928a16eb8304993de53fb0173e2e408e
amal-san <amaldemaniax@gmail.com>
2021-05-04 08:08:02 +0530

    feat(Contributor): add maintainer filter

--- components/contributor.js
+++ components/contributor.js
@@ -30,24 +30,37 @@ class UserStatus {
     }
 }
 
-function Contributor({ name, contributions, avatar, maintainer }) {
+function Contributor({ name, contributions, avatar }) {
+    const [maintainers, setMaintainers] = useState([]);
     const user = new UserStatus(contributions);
+
+    useEffect(() => {
+        fetch("https://api.github.com/orgs/thinkfiveable/public_members")
+            .then((res) => res.json())
+            .then(setMaintainers)
+            .catch((err) => err);
+    }, []);
+
+    const maintainersName = maintainers.map(({ login }) => ({ login }));
+    const checkUserStatus = maintainersName.find(({ login }) => login === name) !== undefined;
 
     return (
         <div className="flex m-4">
             <div className="m-0 mr-5 w-1/4 h-2/4">
-                <img className=" w-4/4 rounded-full" src={avatar} />
+                <img alt="avatar" className=" w-4/4 rounded-full" src={avatar} />
             </div>
             <div className="self-center flex-1">
                 <h1 className=" text-lg font-medium text-left">@{name}</h1>
-                <p
-                    className="m-1 pb-1 h-5 rounded-full text-center text-xs font-medium "
-                    style={{
-                        background: user.getLabelBgColor(maintainer),
-                        color: maintainer ? "white" : "black"
-                    }}>
-                    {user.getLabelText(maintainer)}
-                </p>
+                {maintainersName ? (
+                    <p
+                        className="m-1 pb-1 h-5 rounded-full text-center text-xs font-medium "
+                        style={{
+                            background: user.getLabelBgColor(checkUserStatus),
+                            color: checkUserStatus ? "white" : "black"
+                        }}>
+                        {user.getLabelText(checkUserStatus)}
+                    </p>
+                ) : null}
             </div>
         </div>
     );
@@ -66,10 +79,9 @@ export default function Contributors() {
     return (
         <section className="flex-col p-8 lg:w-2/4" style={{ background: "#e5e5e5", margin: "0 auto" }}>
             {usersDetails
-                ? usersDetails.map((user, i) => (
+                ? usersDetails.map((user) => (
                       <Contributor
                           key={user.id}
-                          maintainer={i === 0}
                           name={user.login}
                           contributions={user.contributions}
                           avatar={user.avatar_url}
